a2dbe4cd7b12d5a3c3f7f79f4de07fb332b68558
p1 <21110608@student.hcmute.edu.vn>
2023-12-07 14:09:46 +0700

    first commit

---
+++ W11/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace W11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_MaSV = new System.Windows.Forms.TextBox();
+            this.txt_TenSV = new System.Windows.Forms.TextBox();
+            this.txt_Khoa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.col_MaSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_MaKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_Khoa);
+            this.groupBox1.Controls.Add(this.txt_TenSV);
+            this.groupBox1.Controls.Add(this.txt_MaSV);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(133, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(604, 218);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin sinh viên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã SV:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên SV:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Khoa:";
+            // 
+            // txt_MaSV
+            // 
+            this.txt_MaSV.Location = new System.Drawing.Point(180, 39);
+            this.txt_MaSV.Name = "txt_MaSV";
+            this.txt_MaSV.Size = new System.Drawing.Size(174, 30);
+            this.txt_MaSV.TabIndex = 3;
+            // 
+            // txt_TenSV
+            // 
+            this.txt_TenSV.Location = new System.Drawing.Point(180, 83);
+            this.txt_TenSV.Name = "txt_TenSV";
+            this.txt_TenSV.Size = new System.Drawing.Size(174, 30);
+            this.txt_TenSV.TabIndex = 4;
+            // 
+            // txt_Khoa
+            // 
+            this.txt_Khoa.Location = new System.Drawing.Point(180, 130);
+            this.txt_Khoa.Name = "txt_Khoa";
+            this.txt_Khoa.Size = new System.Drawing.Size(174, 30);
+            this.txt_Khoa.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(189, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Lưu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(340, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(499, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(167, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tìm kiếm:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(313, 345);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(174, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyUp);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_MaSV,
+            this.col_HoTen,
+            this.col_MaKhoa});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 392);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(827, 159);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // col_MaSV
+            // 
+            this.col_MaSV.DataPropertyName = "MaSV";
+            this.col_MaSV.HeaderText = "Mã SV";
+            this.col_MaSV.MinimumWidth = 6;
+            this.col_MaSV.Name = "col_MaSV";
+            this.col_MaSV.Width = 125;
+            // 
+            // col_HoTen
+            // 
+            this.col_HoTen.DataPropertyName = "HoTen";
+            this.col_HoTen.HeaderText = "Họ Tên";
+            this.col_HoTen.MinimumWidth = 6;
+            this.col_HoTen.Name = "col_HoTen";
+            this.col_HoTen.Width = 125;
+            // 
+            // col_MaKhoa
+            // 
+            this.col_MaKhoa.DataPropertyName = "MaKhoa";
+            this.col_MaKhoa.HeaderText = "Mã Khoa";
+            this.col_MaKhoa.MinimumWidth = 6;
+            this.col_MaKhoa.Name = "col_MaKhoa";
+            this.col_MaKhoa.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 559);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_Khoa;
+        private System.Windows.Forms.TextBox txt_TenSV;
+        private System.Windows.Forms.TextBox txt_MaSV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_MaSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_MaKhoa;
+    }
+}
+
---
+++ W11/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SinhVien sv = new SinhVien();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            sv.MaSV = int.Parse(txt_MaSV.Text);
+            sv.HoTen = txt_TenSV.Text;
+            sv.MaKhoa = txt_Khoa.Text;
+
+            db.SinhViens.InsertOnSubmit(sv);
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+            txt_MaSV.Text = "";
+            txt_TenSV.Text = "";
+            txt_Khoa.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            var lst = (from s in db.SinhViens select s).ToList();
+            dataGridView1.DataSource = lst;
+            txt_MaSV.DataBindings.Clear();
+            txt_TenSV.DataBindings.Clear();
+
+            txt_Khoa.DataBindings.Clear();
+            txt_MaSV.DataBindings.Add("text", lst, "MaSV");
+            txt_TenSV.DataBindings.Add("text", lst, "HoTen");
+
+            txt_Khoa.DataBindings.Add("text", lst, "MaKhoa");
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            sv = db.SinhViens.Where(s => s.MaSV == int.Parse(txt_MaSV.Text)).Single();
+
+            sv.HoTen = txt_TenSV.Text;
+            sv.MaKhoa = txt_Khoa.Text;
+            db.SinhViens.DeleteOnSubmit(sv);
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+
+        }
+
+        private void textBox4_KeyUp(object sender, KeyEventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            var lst = (from s in db.SinhViens where s.MaSV.ToString().Contains(textBox4.Text) select s).ToList();
+            dataGridView1.DataSource = lst;
+            txt_MaSV.DataBindings.Clear();
+            txt_TenSV.DataBindings.Clear();
+
+            txt_Khoa.DataBindings.Clear();
+            txt_MaSV.DataBindings.Add("text", lst, "MaSV");
+            txt_TenSV.DataBindings.Add("text", lst, "HoTen");
+
+            txt_Khoa.DataBindings.Add("text", lst, "MaKhoa");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            sv = db.SinhViens.Where(s => s.MaSV == int.Parse(txt_MaSV.Text)).Single();
+
+            sv.HoTen = txt_TenSV.Text;
+            sv.MaKhoa = txt_Khoa.Text;
+            
+            db.SubmitChanges();
+            Form1_Load(sender, e);
+
+        }
+    }
+}
